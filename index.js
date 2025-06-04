914fabe704c048c8b70206c25fae547815f4ae4b
Romej <claveau.je@gmail.com>
2025-06-05 01:22:20 +0200

    change package / action metadata

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`${useNpm ? "npx --no-install" : "pnpm run"} ${cmd} --${platform} ${
+				`${useNpm ? "pnpm" : "yarn run"} ${cmd} --${platform} ${
 					release ? "--publish always" : ""
 				} ${args}`,
 				appRoot,
